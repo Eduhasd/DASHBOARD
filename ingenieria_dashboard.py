2de73fed838c63bf9798c61e3d451567aa75a15d
Eduhasd <ealarcond2019@alumnos.ceat.cl>
2024-11-27 03:56:26 -0300

    Update ingenieria_dashboard.py

--- ingenieria_dashboard.py
+++ ingenieria_dashboard.py
@@ -1,17 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-
-import subprocess
-import sys
-
-try:
-    import matplotlib.pyplot as plt
-    print("matplotlib está instalado correctamente.")
-except ImportError:
-    print("matplotlib no está instalado. Procediendo a la instalación...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib"])
-    
+import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 # Título del dashboard
